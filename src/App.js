4bd8fe3cd305a797ed932433169048b098930800
Juan Pablo Sala <jpsala@gmail.com>
2023-08-22 18:57:48 -0300

    anda?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.getElementById('editor')?.focus();
+    document.getElementById('editor')?.click();
+    setTimeout(() => {
+      document.getElementById('editor')?.focus();
+      document.getElementById('editor')?.click();
+      console.log('detalle', document.getElementById('editor'));      
+  }, 400);
+    console.log('heyyy');
+  })
   return (
-      <textarea autoFocus></textarea>
+      <textarea id="editor" autoFocus></textarea>
   );
 }
 
